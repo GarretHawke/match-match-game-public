17550fb57812a7d18d562418e46f5a17d0adea52
GarretHawke <alexandra17au@gmail.com>
2021-05-25 07:48:12 +0300

    feat: edit config

--- webpack.config.ts
+++ webpack.config.ts
@@ -75,7 +75,7 @@ const config: Configuration = {
             loader: 'url-loader',
             options: {
               limit: 8192,
-              name: /* isProduction ? '[contenthash].[ext]' : */ '[name].[ext]',
+              name: isProduction ? '[contenthash].[ext]' : '[name].[ext]',
               outputPath: 'static/images',
             },
           },
